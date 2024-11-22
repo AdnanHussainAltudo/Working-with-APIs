48eaa824dc940a2770f3bc9fa0542ab29ae3f525
Md Adnan Hussain <adnan.hussain@altudo.co>
2024-11-22 20:05:23 +0530

    Initial commit

---
+++ app/posts/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center min-h-screen mx-10 font-[family-name:var(--font-geist-sans)] p-6">
+      <p>Loading post details...</p>
+    </div>
+  );
+};
+
+export default loading;
